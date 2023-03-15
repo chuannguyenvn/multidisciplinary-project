39bee6bba2a37df5006124a4cdcc9e726310e0af
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-03-15 17:09:17 +0700

    refactor: prefabs

---
+++ Assets/_Scripts/PlantInformationStamp.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantInformationStamp : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Image background;
+    [SerializeField] private TMP_Text informationText;
+
+    private void Start()
+    {
+        
+    }
+}
