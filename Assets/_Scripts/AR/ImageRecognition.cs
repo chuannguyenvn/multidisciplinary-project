a0dd0379256b3b657990900b76e33b639045d562
QuangBao29 <bao.nguyen2969@gmail.com>
2023-04-27 22:43:08 +0700

    Revert "Auxiliary commit to revert individual files from 5b3a20d8fd100264e55e2ee8973f1c6b97a4a689"
    This reverts commit dedddc27ce86f3ad055283957995a0b4d22d2fa4.
    
    # Conflicts:
    #	Assets/_Scripts/AR/ImageRecognition.cs

--- Assets/_Scripts/AR/ImageRecognition.cs
+++ Assets/_Scripts/AR/ImageRecognition.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.UI;
-using TMPro;
-
 public class ImageRecognition : MonoBehaviour
 {
     private ARTrackedImageManager _arTrackedImageManager;
     [SerializeField]
     private GameObject _panel = null;
-    //[SerializeField]
-    //private TextMeshProUGUI _text = null;
 
     private void Awake()
     {
@@ -26,38 +20,16 @@ public class ImageRecognition : MonoBehaviour
     public void OnDisable()
     {
         _arTrackedImageManager.trackedImagesChanged -= OnShowPanel;
+        _arTrackedImageManager.trackedImagesChanged += OnHidePanel;
     }
 
     public void OnShowPanel(ARTrackedImagesChangedEventArgs args)
     {
         _panel.SetActive(true);
-        //var str = "";
-        //foreach (var trackImage in _arTrackedImageManager.trackables)
-        //{
-        //    Debug.LogError("trackImage name: " + trackImage.referenceImage.name);
-        //    str += trackImage.referenceImage.name;
-        //}
-        //_text.text = str;
-        //foreach (var evt in args.added)
-        //{
-        //    Debug.LogError("evt name: " + evt.referenceImage.name);
-        //}
     }
-    //public void OnClickTest()
-    //{
-    //    Debug.LogError("on click test");
-    //    AddImage(_img.sprite.texture);
-    //}
-    //void AddImage(Texture2D imageToAdd)
-    //{
-    //    Debug.LogError("on add img");
-    //    var library = _arTrackedImageManager.CreateRuntimeLibrary();
-    //    if (library is MutableRuntimeReferenceImageLibrary mutableLibrary)
-    //    {
-    //        mutableLibrary.ScheduleAddImageWithValidationJob(
-    //            imageToAdd,
-    //            "my new image",
-    //            0.5f /* 50 cm */);
-    //    }
-    //}
+    public void OnHidePanel(ARTrackedImagesChangedEventArgs args)
+    {
+        _panel.SetActive(false);
+    }
+
 }
